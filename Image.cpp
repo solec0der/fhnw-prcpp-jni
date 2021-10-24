72c5e77f3bb2c569301a8401244e4cafc928135a
Yannick Huggler <yannick@huggler.io>
2021-10-24 18:32:55 +0200

    Initial Commit

---
+++ Image.cpp
@@ -0,0 +1,12 @@
+#include "jni.h"
+#include "Image.h"
+
+JNIEXPORT void JNICALL Java_Image_inverting
+        (JNIEnv *, jclass, jint, jint, jintArray) {
+
+}
+
+JNIEXPORT void JNICALL Java_Image_filtering
+        (JNIEnv *, jclass, jint, jint, jintArray, jintArray, jintArray) {
+
+}
